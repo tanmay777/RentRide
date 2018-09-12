f8bd2ca791c1a745e725cfe39d1fcad78e49d96f
Tanmay Jha <tanmay.jha1@gmail.com>
2018-09-12 17:08:47 +0530

    Basic Architecture Laid

---
+++ app/src/main/java/com/example/tanmay/rentbaazvehicleadministration/Entity/Home/HomeActivity.kt
@@ -0,0 +1,31 @@
+package com.example.tanmay.rentbaazvehicleadministration.Entity.Home
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import com.example.tanmay.rentbaazvehicleadministration.R
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_all_vehicles -> {
+                message.setText(R.string.title_all_vehicles)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_manage_sessions -> {
+                message.setText(R.string.title_manage_sessions)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
