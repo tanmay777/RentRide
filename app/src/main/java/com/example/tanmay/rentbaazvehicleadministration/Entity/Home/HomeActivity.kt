8429e2558376495aeb4d0c2c3db342b74a4255e1
Tanmay Jha <tanmay.jha1@gmail.com>
2018-10-03 18:35:44 +0530

    Tabbed View completed wrt designs

--- app/src/main/java/com/example/tanmay/rentbaazvehicleadministration/Entity/Home/HomeActivity.kt
+++ app/src/main/java/com/example/tanmay/rentbaazvehicleadministration/Entity/Home/HomeActivity.kt
@@ -3,13 +3,18 @@ package com.example.tanmay.rentbaazvehicleadministration.Entity.Home
 import android.support.v7.app.AppCompatActivity
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.TabLayout
 import android.text.Html
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.LinearLayout
 
 import com.example.tanmay.rentbaazvehicleadministration.R
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.nav_tab.view.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -27,34 +32,56 @@ class HomeActivity : AppCompatActivity() {
 
     private fun configureTabLayout() {
         //This creates four tabs and assigns text to them
-        /*
-        val tab = LayoutInflater.from(this).inflate(R.layout.nav_tab,null) as CoordinatorLayout
-        tab.nav_label.text="All Vehicles"
-        tab.nav_icon.setImageResource(R.drawable.ic_car)
-        tab.nav_icon.alpha=0.5f
-         */
-        tab_layout.addTab(tab_layout.newTab().setText("All Vehicles").setIcon(R.drawable.ic_car))
+
+        tab_layout.addTab(tab_layout.newTab())
+        tab_layout.addTab(tab_layout.newTab())
+        tab_layout.addTab(tab_layout.newTab())
+
+        val tab = LayoutInflater.from(this).inflate(R.layout.nav_tab, null) as LinearLayout
+        tab.nav_label.text = "All"
+        tab.nav_label.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_car,0,0,0)
+        tab.nav_label.scaleY=-1f;
+        tab_layout.getTabAt(0)!!.setCustomView(tab)
+
+        val tab1 = LayoutInflater.from(this).inflate(R.layout.nav_tab, null) as LinearLayout
+        tab1.nav_label.text = "Available"
+        tab1.nav_label.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_car,0,0,0)
+        tab1.nav_label.scaleY=-1f;
+        //tab1.nav_label.alpha=.5f
+        tab_layout.getTabAt(1)!!.setCustomView(tab1)
+        tab_layout.getTabAt(1)!!.customView?.alpha=.5f
+
+        val tab2 = LayoutInflater.from(this).inflate(R.layout.nav_tab, null) as LinearLayout
+        tab2.nav_label.text = "On Rent"
+        tab2.nav_label.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_car,0,0,0)
+        //tab2.nav_label.alpha=.5f
+        tab2.nav_label.scaleY=-1f;
+        tab_layout.getTabAt(2)!!.setCustomView(tab2)
+        tab_layout.getTabAt(2)!!.customView?.alpha=.5f
+
+        /*tab_layout.addTab(tab_layout.newTab().setText("All Vehicles").setIcon(R.drawable.ic_car))
         val tab2=tab_layout.newTab().setText("Available").setIcon(R.drawable.ic_car)
         tab2.icon?.alpha=128
         tab_layout.addTab(tab2)
         val tab3=tab_layout.newTab().setText("On Rent").setIcon(R.drawable.ic_car)
         tab3.icon?.alpha=128
         tab_layout.addTab(tab3)
+       */
 
-        val adapter=HomeTabPagerAdapter(supportFragmentManager,tab_layout.tabCount)
+        val adapter = HomeTabPagerAdapter(supportFragmentManager, tab_layout.tabCount)
 
-        pager.adapter=adapter
+        pager.adapter = adapter
 
         pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
 
-        tab_layout.addOnTabSelectedListener(object:TabLayout.OnTabSelectedListener{
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
-                pager.currentItem=tab.position
-                tab.icon?.alpha=255
+                pager.currentItem = tab.position
+                tab.customView?.alpha=1.0f
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
-                tab?.icon?.alpha=128
+                tab?.customView?.alpha=0.5f
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
@@ -64,15 +91,15 @@ class HomeActivity : AppCompatActivity() {
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+// Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_home_activityone, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+// Handle action bar item clicks here. The action bar will
+// automatically handle clicks on the Home/Up button, so long
+// as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
 
         if (id == R.id.action_settings) {
@@ -87,5 +114,5 @@ class HomeActivity : AppCompatActivity() {
         finishAffinity()
     }
 
-    //TODO: Add perimission mode for firestore database
+//TODO: Add perimission mode for firestore database
 }
