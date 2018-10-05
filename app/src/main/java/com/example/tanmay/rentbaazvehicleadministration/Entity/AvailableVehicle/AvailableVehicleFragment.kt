4bdde204330440f7215d88409425b54c38db6ced
Tanmay Jha <tanmay.jha1@gmail.com>
2018-10-05 15:12:32 +0530

    Firestore recycler view for Available and On rent fragement is added

--- app/src/main/java/com/example/tanmay/rentbaazvehicleadministration/Entity/AvailableVehicle/AvailableVehicleFragment.kt
+++ app/src/main/java/com/example/tanmay/rentbaazvehicleadministration/Entity/AvailableVehicle/AvailableVehicleFragment.kt
@@ -3,28 +3,86 @@ package com.example.tanmay.rentbaazvehicleadministration.Entity.AvailableVehicle
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.tanmay.rentbaazvehicleadministration.Entity.AllVehicle.AllVehicleFragment
+import com.example.tanmay.rentbaazvehicleadministration.Entity.AllVehicle.AllVehicleModel
 
 import com.example.tanmay.rentbaazvehicleadministration.R
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.fragment_all_vehicle.view.*
+import kotlinx.android.synthetic.main.fragment_available_vehicle.view.*
+import kotlinx.android.synthetic.main.vehicle_detail_card.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class AvailableFragment : Fragment() {
+
+    lateinit var fragmentView: View
+    private var availableVehicleFirestoreRecyclerAdapter: AvailableVehicleFirestoreRecyclerAdapter?=null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_available_vehicle, container, false)
+        fragmentView=inflater.inflate(R.layout.fragment_available_vehicle, container, false)
+        return fragmentView
     }
 
+    override fun onStart() {
+        super.onStart()
+        fragmentView.recycler_view_available_vehicle.layoutManager= LinearLayoutManager(fragmentView.context)
+        //create the root reference of your Firestore database
+        val rootRef= FirebaseFirestore.getInstance()
+        val query=rootRef!!.collection("available_vehicle") .orderBy("vehicle_name", Query.Direction.ASCENDING)
+        val options = FirestoreRecyclerOptions.Builder<AvailableVehicleModel>().setQuery(query, AvailableVehicleModel::class.java).build()
+        availableVehicleFirestoreRecyclerAdapter=AvailableVehicleFirestoreRecyclerAdapter(options)
+        fragmentView.recycler_view_available_vehicle.adapter=availableVehicleFirestoreRecyclerAdapter
+
+        if (availableVehicleFirestoreRecyclerAdapter!= null) {
+            availableVehicleFirestoreRecyclerAdapter!!.startListening()
+        }
+    }
+
+    inner class VehicleViewHolder internal constructor(private val view:View): RecyclerView.ViewHolder(view){
+        internal fun setVehicleInfo(availableVehicleModel: AvailableVehicleModel){
+            view.bike_name.text=availableVehicleModel.vehicle_name
+            view.bike_number.text=availableVehicleModel.vehicle_number
+            Glide.with(fragmentView).load(availableVehicleModel.vehicle_image_url).into(view.bike_img)
+            view.bike_vendor_organization.text=availableVehicleModel.vendor_organization
+            view.cost_weekday.text=availableVehicleModel.weekday_cost
+            view.cost_weekend.text=availableVehicleModel.weekend_cost
+        }
+    }
+
+    inner class AvailableVehicleFirestoreRecyclerAdapter internal constructor(options:FirestoreRecyclerOptions<AvailableVehicleModel>): FirestoreRecyclerAdapter<AvailableVehicleModel, VehicleViewHolder>(options){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehicleViewHolder {
+            val view=LayoutInflater.from(parent.context).inflate(R.layout.vehicle_detail_card,parent,false)
+            return VehicleViewHolder(view)
+        }
+
+        override fun onBindViewHolder(vehicleViewHolder: VehicleViewHolder, position: Int, availableVehicleModel: AvailableVehicleModel) {
+            vehicleViewHolder.setVehicleInfo(availableVehicleModel)
+        }
+
+        override fun onDataChanged() {
+            fragmentView.recycler_view_available_vehicle.layoutManager?.scrollToPosition(itemCount - 1)
+            //TODO: Add a refresh sign too
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (availableVehicleFirestoreRecyclerAdapter!= null) {
+            availableVehicleFirestoreRecyclerAdapter!!.stopListening()
+        }
+    }
 
 }
--- app/src/main/java/com/example/tanmay/rentbaazvehicleadministration/Entity/OnRentVehicle/OnRentVehicleFragment.kt
+++ app/src/main/java/com/example/tanmay/rentbaazvehicleadministration/Entity/OnRentVehicle/OnRentVehicleFragment.kt
@@ -3,28 +3,85 @@ package com.example.tanmay.rentbaazvehicleadministration.Entity.OnRentVehicle
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.tanmay.rentbaazvehicleadministration.Entity.AllVehicle.AllVehicleFragment
+import com.example.tanmay.rentbaazvehicleadministration.Entity.AllVehicle.AllVehicleModel
 
 import com.example.tanmay.rentbaazvehicleadministration.R
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.fragment_all_vehicle.view.*
+import kotlinx.android.synthetic.main.fragment_on_rent_vehicle.view.*
+import kotlinx.android.synthetic.main.vehicle_detail_card.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class OnRentVehicleFragment : Fragment() {
+
+    lateinit var fragmentView: View
+    private var onRentVehicleFirestoreRecyclerAdapter: OnRentVehicleFirestoreRecyclerAdapter?=null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_rent_vehicle, container, false)
+        fragmentView=inflater.inflate(R.layout.fragment_on_rent_vehicle, container, false)
+        return fragmentView
     }
 
+    override fun onStart() {
+        super.onStart()
+        fragmentView.recycler_view_on_rent.layoutManager= LinearLayoutManager(fragmentView.context)
+        //create the root reference of your Firestore database
+        val rootRef= FirebaseFirestore.getInstance()
+        val query=rootRef!!.collection("rented_vehicle") .orderBy("vehicle_name", Query.Direction.ASCENDING)
+        val options = FirestoreRecyclerOptions.Builder<OnRentVehicleModel>().setQuery(query, OnRentVehicleModel::class.java).build()
+        onRentVehicleFirestoreRecyclerAdapter=OnRentVehicleFirestoreRecyclerAdapter(options)
+        fragmentView.recycler_view_on_rent.adapter=onRentVehicleFirestoreRecyclerAdapter
 
+        if (onRentVehicleFirestoreRecyclerAdapter!= null) {
+            onRentVehicleFirestoreRecyclerAdapter!!.startListening()
+        }
+    }
+
+    inner class VehicleViewHolder internal constructor(private val view:View): RecyclerView.ViewHolder(view){
+        internal fun setVehicleInfo(onRentVehicleModel: OnRentVehicleModel){
+            view.bike_name.text=onRentVehicleModel.vehicle_name
+            view.bike_number.text=onRentVehicleModel.vehicle_number
+            Glide.with(fragmentView).load(onRentVehicleModel.vehicle_image_url).into(view.bike_img)
+            view.bike_vendor_organization.text=onRentVehicleModel.vendor_organization
+            view.cost_weekday.text=onRentVehicleModel.weekday_cost
+            view.cost_weekend.text=onRentVehicleModel.weekend_cost
+        }
+    }
+
+    inner class OnRentVehicleFirestoreRecyclerAdapter internal constructor(options:FirestoreRecyclerOptions<OnRentVehicleModel>): FirestoreRecyclerAdapter<OnRentVehicleModel, VehicleViewHolder>(options){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehicleViewHolder {
+            val view=LayoutInflater.from(parent.context).inflate(R.layout.vehicle_detail_card,parent,false)
+            return VehicleViewHolder(view)
+        }
+
+        override fun onBindViewHolder(vehicleViewHolder: VehicleViewHolder, position: Int, onRentVehicleModel: OnRentVehicleModel) {
+            vehicleViewHolder.setVehicleInfo(onRentVehicleModel)
+        }
+
+        override fun onDataChanged() {
+            fragmentView.recycler_view_on_rent.layoutManager?.scrollToPosition(itemCount - 1)
+            //TODO: Add a refresh sign too
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (onRentVehicleFirestoreRecyclerAdapter!= null) {
+            onRentVehicleFirestoreRecyclerAdapter!!.stopListening()
+        }
+    }
 }
